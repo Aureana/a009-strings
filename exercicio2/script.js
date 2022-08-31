bea3aa45d6c25772cebfb40791e0f8eb15befc13
Aureana <111321782+Aureana@users.noreply.github.com>
2022-08-31 16:50:23 -0300

    Atividade Resolvida

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,13 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+console.log(minhaString)
+
+console.log("Antes da remoção do espaço tinham:", +minhaString.length+ " caracteres e depois ficou " +minhaString.trim().length+ " e \n"  + minhaString.replace("______","sticioso"))
+
+/*
+console.log(minhaString.trim())
+
+const quantidadeCaracteres = minhaString.length
+console.log(quantidadeCaracteres)*/
+
+
+//console.log(`Olá, Seu nome é: ${nome} e sua cor favorita é ${cor}`)
